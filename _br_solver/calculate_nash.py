5ee4595c0ab52253471e9726fc26ee0df11ad3b6
DCM <dcmay@ualberta.ca>
2020-10-13 15:16:26 -0600

    some DB acess

--- _br_solver/calculate_nash.py
+++ _br_solver/calculate_nash.py
@@ -1,9 +1,38 @@
 # The goal of this is to calculate the emerging nash equilibrium from a given simulation state
 # ----------------------------------------------------------------------------------------------------------------------
+import pandas as pd
+from _extractor.extractor import Extractor
+from sqlalchemy import create_engine
+
+
+def get_tables(engine):
+    find_profile_names = """
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = 'public'
+    ORDER BY table_name
+    """
+    table = pd.read_sql_query(find_profile_names, engine) # get tables
+
+    return [element[0] for element in table.values.tolist()] #unpack, because list of lists
+
 
 # Get the data
     # acess a simulation database
     # import the profiles of all participants
+
+db_path1 = 'postgresql://postgres:postgres@stargate/remote_agent_test_np'
+table_list = get_tables(create_engine(db_path1))
+
+agent_id = 'egauge19821'
+sim_type = 'training'
+start_gen = 0
+
+extractor = Extractor(db_path1)
+agent_market_df = extractor.from_metrics(start_gen, sim_type, agent_id)
+
+# engine = create_engine('postgresql://postgres:postgres@stargate/profiles')
+
 
 # Construct an MPD
     # construct an MDP for each of the participants
